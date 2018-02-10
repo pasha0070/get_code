f01874b40d2d56dae2b197dbb7910a2c935f01a1
pasha0070 <shidlovsckiy.p@yandex.ru>
2018-02-10 19:36:40 +0200

    add task

---
+++ getproxy/getproxy/resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!-- resources/views/layouts/app.blade.php -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Laravel Quickstart - Intermediate</title>
+
+	<!-- CSS и JavaScript -->
+</head>
+
+<body>
+<div class="container">
+	<nav class="navbar navbar-default">
+		<!-- Содержимое Navbar -->
+	</nav>
+</div>
+
+@yield('content')
+</body>
+</html>
